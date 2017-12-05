6f878b8890db3292ff7947208c0e39ddf8925c67
Md. Sazibur Rahman <sajibrahman.spi@gmail.com>
2017-12-05 23:57:08 +0600

    Capture-with-Camera

---
+++ CaptureWithCamera/app/src/main/java/com/techsajib/capturewithcamera/MainActivity.java
@@ -0,0 +1,43 @@
+package com.techsajib.capturewithcamera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+    Button capture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.imageView);
+        capture = (Button) findViewById(R.id.button);
+
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        image.setImageBitmap(bitmap);
+    }
+}
